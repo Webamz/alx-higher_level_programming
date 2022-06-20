d9b0fbd7259769664b73f5617266743366f259d0
Webamz <chriszxn99@gmail.com>
2022-06-20 22:49:36 +0000

    exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
 
     try:
         response = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, FloatingPointError):
         response = None
     finally:
         print('Inside result: {}'.format(response))
