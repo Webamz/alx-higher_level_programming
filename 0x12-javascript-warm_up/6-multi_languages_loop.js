ac5006e482b9298669d710e8133150f38086c246
Webamz <chriszxn99@gmail.com>
2023-06-12 12:23:41 +0200

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const out = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const out = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < out.length; i++) {
   console.log(out[i]);
