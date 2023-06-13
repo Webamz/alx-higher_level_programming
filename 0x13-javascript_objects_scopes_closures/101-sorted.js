ddd761da2608528960165d7750b98d3bae0bd198
Webamz <chriszxn99@gmail.com>
2023-06-13 12:51:12 +0200

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,7 +4,7 @@ const dict = require('./101-data.js').dict;
 const newDict = {};
 
 for (const key in dict) {
-  if (newDict[dict[key]] ==== undefined) {
+  if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
