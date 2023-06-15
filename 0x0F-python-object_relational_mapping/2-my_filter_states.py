b44b052d403a83503c526b3ca85f5a215963f92d
Webamz <chriszxn99@gmail.com>
2023-06-15 12:56:49 +0200

    orm

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Displays states that match the name argument"""
+
+import MySQLdb
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+state_name = sys.argv[4]
+
+db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE states.name='{}' ORDER BY states.id ASC".format(state_name))
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
