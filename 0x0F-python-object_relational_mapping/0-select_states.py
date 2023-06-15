cfd09ebbb79d6be4aadc31a4f9ffaf1273d703e4
Webamz <chriszxn99@gmail.com>
2023-06-15 12:07:51 +0200

    orm1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" lists all states from database hbtn_0e_o_usa"""
+
+import MySQLdb
+import sys
+
+usr = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+db = MySQLdb.connect(host='localhost', port=3306, user=usr, passwd=password, db=database) 
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
