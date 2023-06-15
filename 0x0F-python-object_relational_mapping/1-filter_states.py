964940e8007ab6ab64432485d2aca4241021fea0
Webamz <chriszxn99@gmail.com>
2023-06-15 12:22:50 +0200

    orm

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Lists states with name starting with N"""
+
+import MySQLdb
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
