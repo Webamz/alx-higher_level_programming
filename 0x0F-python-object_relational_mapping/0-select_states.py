d5c6d1c1132300aafba77f57fdf450c9508d1c2e
Webamz <chriszxn99@gmail.com>
2023-06-17 12:14:14 +0200

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3]) 
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-            ORDER BY states.id ASC;")
+            ORDER BY id ASC;")
     rows = cur.fetchall()
 
     for row in rows:
