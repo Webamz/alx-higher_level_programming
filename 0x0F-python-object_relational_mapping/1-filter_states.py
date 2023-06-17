d050ae5ab33c70b4df9704963ce59963968f0d9e
Webamz <chriszxn99@gmail.com>
2023-06-17 13:04:38 +0200

    pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db = MySQLdb.connect(host='localhost', port=3306, 
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=username, passwd=password, db=database)
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost', port=3306, 
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=username, passwd=password, db=database)
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
