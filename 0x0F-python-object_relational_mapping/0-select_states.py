d264f7b04f4a95f02057acc28bcb7371e15a4b7f
Webamz <chriszxn99@gmail.com>
2023-06-17 11:49:15 +0200

    spaces

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,11 @@ usr = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', port=3306, user=usr, passwd=password, db=database) 
+db = MySQLdb.connect(host='localhost', port=3306, user=usr,\
+                     passwd=password, db=database) 
 cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+cur.execute("SELECT * FROM states\
+            ORDER BY states.id ASC;")
 rows = cur.fetchall()
 
 for row in rows:
