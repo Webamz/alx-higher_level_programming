37c1f5053aedf4bea01991505a73094cdb213c01
Webamz <chriszxn99@gmail.com>
2023-06-17 12:20:48 +0200

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          db=sys.argv[3], port=3306) 
     cur = db.cursor()
     cur.execute("SELECT * FROM states;")
-    rows = cur.fetchall()
+    states = cur.fetchall()
 
-    for row in rows:
-        print(row)
+    for state in states:
+        print(state)
