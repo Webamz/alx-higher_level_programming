1c91499c83aadfe1f526100dfe1f9e9d2bf8e4f2
Webamz <chriszxn99@gmail.com>
2023-06-18 11:51:28 +0200

    first object(state)

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,7 +4,6 @@
 
 if __name__ == '__main__':
     import sys
-    import sqlalchemy
     from model_state import State, Base
     from sqlalchemy.orm import Session
     from sqlalchemy import create_engine
@@ -12,7 +11,7 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3], pool_pre_ping=True))
-    
+
     Base.metadata.create_all(engine)
 
     session = Session(engine)
