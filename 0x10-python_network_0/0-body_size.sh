dc2e5a71f26a5a45e32dbef2d22d403532d4dc1e
Webamz <chriszxn99@gmail.com>
2023-06-29 14:18:46 +0200

    body size

--- 0x10-python_network_0/0-body_size.sh
+++ 0x10-python_network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # script that takes url, sends a request
 # and displays size of the body of the response
 
-curl -sI "$1" | grep 'Content-length' | awk '{print $2}'
+curl -sI "$1" | grep 'Content-Length' | cut -d " " -f 2
