1724fc95b1df8de8a233e063a3cfa65705f9a8a7
Webamz <chriszxn99@gmail.com>
2023-06-30 20:42:42 +0200

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as first argument
-# and displays body of the response
-curl -sX POST -H "Content-Type: application/json" -d @"$2"
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
